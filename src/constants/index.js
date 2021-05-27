65385543441acc7e5c62be65f7a85eefa68c1578
hyerichung <hyerichungonearth@gmail.com>
2021-05-28 05:10:56 +0900

    [ADD] bottom tab color constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,6 +11,11 @@ export const BOTTOM_TAB_ICON = {
   My: "user",
 };
 
+export const BOTTOM_TAB_ICON_COLOR = {
+  Focused: "#0652DD",
+  Default: "black",
+};
+
 export const URI = {
   ALT: "https://p.scdn.co/mp3-preview/bc5f3e28ba28c76b36f409d3c3f697e597b6ff6f?cid=41014a1f3ad143a8be14a47f025c209d",
 };
